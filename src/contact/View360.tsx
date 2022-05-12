1437c8cc47e3585a17fed52bfa69a09a12f6918a
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2022-05-12 18:54:55 +0200

    Using Dropdown from FluenUI

--- src/contact/View360.tsx
+++ src/contact/View360.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import { Dropdown, IDropdownOption } from '@fluentui/react';
 import { IView360Props } from '@ppuga/demo/contact/View360.types';
+
+const dropdownOptions: IDropdownOption[] = [
+    { key: 0, text: 'Is this cool?' },
+    { key: 1, text: 'It must be cool, right?' }
+]
+
 
 const View360 = ({ isNew }: IView360Props) =>
     <p>
         {isNew ?
             <p>Creating contact</p> :
-            <p>Editing contact </p>
+            <p>Editing contact : <Dropdown options={dropdownOptions} /></p>
         }
     </p>
 
